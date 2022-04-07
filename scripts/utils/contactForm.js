a9fef3ff7ce8103bebd802515dbf683751433013
Fabienne LIS <fabienne.lis@gmail.com>
2022-04-07 12:00:09 +0200

    ajout

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,23 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+//open modal
+const openModal = document.querySelector(".photograph-header__contact_button");/*button*/
+console.log(openModal);
+const modalForm = document.querySelector(".contact_modal");/*modal*/
+console.log(modalForm);
+
+openModal.addEventListener("click", inputOpenModal);
+
+
+function inputOpenModal() {
+    modalForm.style.display = "block";
+  }
+
+
+//closeModal
+const closeModal = document.querySelector(".close");
+
+closeModal.addEventListener("click", inputCloseModal);
+
+function inputCloseModal() {
+    modalForm.style.display = "none";
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
-}
